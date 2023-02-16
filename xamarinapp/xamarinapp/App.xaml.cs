74ab744e12992d18f031db20919a0e6fca976988
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-02-16 12:22:10 +0100

    add xamarin sample

---
+++ xamarinapp/xamarinapp/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            //here I set some example property
+            Task.Run(async () =>
+            {
+                if (!Properties.ContainsKey("test"))
+                {
+                    Properties.Add("test", "test");
+                    await SavePropertiesAsync();
+                }
+            });
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
