f6baba34a38d988e94d21e89cc880e4579acd59e
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-02-16 12:59:35 +0100

    add maui project

---
+++ mauiapp/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace mauiapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            //This code does not compile in maui, becusase of being obsolete
+            //Properties.TryGetValue("test", out object value);
+            //
+
+            //This code works, but does not access my old properties
+            var test = Preferences.Get("test", default(string));
+        }
+    }
+}
